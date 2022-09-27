16f30de99096db7ed235848b5fe0ebe240628ab3
Bice93 <bianca.cerulo@hotmail.it>
2022-09-27 15:25:46 +0200

    Edit show on postController in api folder

--- app/Http/Controllers/Api/PostController.php
+++ app/Http/Controllers/Api/PostController.php
@@ -54,7 +54,12 @@ class PostController extends Controller
      */
     public function show($id)
     {
-        //
+        $post = Post::findOrFail($id);
+
+        return response()->json([
+            'response' => true,
+            'result' => $post,
+        ]);
     }
 
     /**
